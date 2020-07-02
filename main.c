ad55bb4f3f51f6043f0550ee05cb4f3979d6a09f
kenan <kenan@kajkus.eu>
2020-07-02 21:48:25 +0200

    Reverse calibration pin logic

--- main.c
+++ main.c
@@ -109,12 +109,12 @@ void setup()
 
 void calibration()
 {
-	if((PIND & (1 << PIND5)) != (1 << PIND5)) {
+	if((PIND & (1 << PIND5)) == (1 << PIND5)) {
 		return;
 	}
 	calibrationHello(&u8g);
 	wait(1000);
-	while ((PIND & (1 << PIND5)) != (1 << PIND5))
+	while ((PIND & (1 << PIND5)) == (1 << PIND5))
 	{
 	}
 
@@ -126,7 +126,7 @@ void calibration()
 		{
 			wait(50);
 			sampleAnalogInput(&triggerButton);
-			if((PIND & (1 << PIND5)) != (1 << PIND5)) {
+			if((PIND & (1 << PIND5)) == (1 << PIND5)) {
 				seconds = 6;
 			}
 		}
@@ -136,7 +136,7 @@ void calibration()
 	//int a = getValue(&triggerButton);
 	
 	calibrateReslease(&u8g);
-	while (PIND & (1 << PIND5))
+	while ((PIND & (1 << PIND5)) != (1 << PIND5))
 	{
 	}
 
@@ -148,9 +148,9 @@ void calibration()
 		{
 			wait(50);
 			sampleAnalogInput(&triggerButton);
-			if(PIND & (1 << PIND5)) 
+			if((PIND & (1 << PIND5)) != (1 << PIND5)) 
 			{
-				seconds = 5;
+				seconds = 6;
 			}
 		}
 		seconds--;
